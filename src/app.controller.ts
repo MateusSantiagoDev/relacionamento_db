6cbbe319d3afedb069efa7a08bf1370c9bd7a267
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-12-17 19:29:50 -0300

    feat: implemented swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
+@ApiTags('Status')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOperation({
+    summary: 'Status da aplicação'
+  })
   @Get()
   getAppStatus(): string {
     return this.appService.getAppStatus();
