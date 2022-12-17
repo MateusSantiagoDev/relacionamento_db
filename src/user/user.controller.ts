95293a258895163fcff7f32a9da61c21f76e8199
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-12-17 19:57:13 -0300

    feat(user): implemented controller methods

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,63 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Get, Post, Param, Patch, Delete } from '@nestjs/common';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserEntity } from './entities/user.entity';
+import { UserService } from './user.service';
 
+@ApiTags('User')
 @Controller()
 export class UserController {
-    constructor(private readonly service) {}
+  constructor(private readonly service: UserService) {}
+
+  @ApiOperation({
+    summary: 'adicionar um novo usuário'
+  })
+  @Post()
+  create(@Body() dto: CreateUserDto): Promise<UserEntity> {
+    try {
+        return this.service.create(dto)
+    } catch (err) {}
+  }
+
+  @ApiOperation({
+    summary: 'Visualizar todos os usuários'
+  })
+  @Get()
+  findAll(): Promise<UserEntity[]>{
+    try {
+      return this.service.findAll()
+    } catch (err) {}
+  }
+
+  @ApiOperation({
+    summary: 'Buscar um usuário pelo ID'
+  })
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<UserEntity> {
+    try {
+      return this.service.findOne(id)
+    } catch (err) {}
+  }
+
+  @ApiOperation({
+    summary: 'Editar um usuário pelo ID'
+  })
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() dto: UpdateUserDto): Promise<UserEntity> {
+    try {
+      return this.service.update(id, dto)
+    } catch (err) {}
+  }
+
+  @ApiOperation({
+    summary: 'Remover um usuário pelo ID'
+  })
+  @Delete(':id')
+  delete(@Param('id') id: string): Promise<UserEntity> {
+    try {
+      return this.service.delete(id)
+    } catch (err) {}
+  }
+
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,23 @@
 import { Injectable } from "@nestjs/common";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
 
 @Injectable()
 export class UserService {
+    delete(id: string): Promise<import("./entities/user.entity").UserEntity> {
+      throw new Error('Method not implemented.');
+    }
+    update(id: string, dto: UpdateUserDto): Promise<import("./entities/user.entity").UserEntity> {
+      throw new Error('Method not implemented.');
+    }
+    findOne(id: string): Promise<import("./entities/user.entity").UserEntity> {
+      throw new Error('Method not implemented.');
+    }
+    findAll(): Promise<import("./entities/user.entity").UserEntity[]> {
+      throw new Error('Method not implemented.');
+    }
+    create(dto: CreateUserDto): Promise<import("./entities/user.entity").UserEntity> {
+      throw new Error('Method not implemented.');
+    }
     constructor(private readonly repository) {}
 }
