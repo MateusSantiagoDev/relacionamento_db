a0a737c19436610d3091fed610fea769eaee1157
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-12-20 12:33:26 -0300

    feat(order): implemented 'entity' and 'dto'

--- src/order/dto/create-order.dto.ts
+++ src/order/dto/create-order.dto.ts
@@ -1,1 +1,27 @@
-export class CreateOrderDto {}
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateOrderDto {
+    @ApiProperty({
+      description: 'Nome do produto',
+      example: 'Pizza de mussarela'
+    })
+  name: string;
+
+  @ApiProperty({
+    description: 'Descrição do produto',
+    example: 'Mussarela, oregano, tomate e borda recheada com catupiry'
+  })
+  description: string;
+
+  @ApiProperty({
+    description: 'Preço do produto',
+    example: 35.50,
+  })
+  price: number;
+
+  @ApiProperty({
+    description: 'Imagem do produto',
+    example: 'https://docplayer.com.br/docs-images/91/107684195/images/3-0.jpg'
+  })
+  image: string;
+}
